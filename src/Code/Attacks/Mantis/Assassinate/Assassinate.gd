06ce151ece38f94fa807ff13895e095b7e824e99
Yonatan Attali <yonatan.attali@gmail.com>
2021-11-07 21:20:33 -0500

    Added animation for scythe and assassinate

--- src/Code/Attacks/Mantis/Assassinate/Assassinate.gd
+++ src/Code/Attacks/Mantis/Assassinate/Assassinate.gd
@@ -2,10 +2,18 @@ extends Area2D
 
 
 var damage: float = 10
+var input_lock: float
 
 
 func get_class(): return "Assassinate"
 
+
+func _ready():
+	var anim = $Sprite.animation
+	var new_fps = $Sprite.frames.get_frame_count(anim) / $Timer.wait_time
+	$Sprite.frames.set_animation_speed(anim, new_fps)
+	$Sprite.play(anim)
+	input_lock = $Timer.wait_time
 
 
 func can_damage(_enemy_hit: Node2D):
