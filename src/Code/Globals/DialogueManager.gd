30096706ec4f296c77fc1d61bea1a01d7dc03d31
desultisoft <desultisoft@gmail.com>
2021-11-30 13:43:04 -0800

    Changed a significant amount of dialog flow and code
    Dialog and pausing now no longer fight to pause/unpause the game.
    Dialog has been changed to inform the player they can use E to interact with the NPCs.
    Dialog colliders are now harder to avoid to force story flow.
    Character portraits now work in dialog.
    Dialog can no longer stack.

--- src/Code/Globals/DialogueManager.gd
+++ src/Code/Globals/DialogueManager.gd
@@ -3,23 +3,26 @@ extends Node
 
 signal dialogue_started
 
+var isOn = false
 
 func _ready():
 	pause_mode = PAUSE_MODE_PROCESS
 
 
 func start_dialogue(dialogue_name: String):
+	if(isOn):
+		return 
+	print("ON")
+	isOn = true
 	var conversation = Dialogic.start(dialogue_name)
 	conversation.connect("tree_exited", self, "on_conversation_complete")
-	get_tree().paused = true
 	add_child(conversation)
 	emit_signal("dialogue_started")
 
 
 func on_conversation_complete():
-	var tree = get_tree()
-	if tree:
-		tree.paused = false
+	print("Off")
+	isOn = false
 
 
 func get_skillpoint():
